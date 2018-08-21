90f19dfac08f7e280ab9470e15c3407ee4b46e3b
Michael Gutteridge <michael.gutteridge@gmail.com>
2018-08-21 12:32:18 -0700

    Update host to IP address for database user

--- recipes/database.rb
+++ recipes/database.rb
@@ -17,11 +17,11 @@ execute 'create slurm user' do
   command '/usr/bin/mysql -u root ' \
     "-p#{node['slurm_test_cluster']['mysql_password']} " \
     '-D mysql -r -B -N -h 127.0.0.1 -e ' \
-    '"CREATE USER \'slurm\'@\'localhost\' ' \
+    '"CREATE USER \'slurm\'@\'127.0.0.1\' ' \
     "IDENTIFIED BY '#{node['slurm_test_cluster']['mysql_password']}'\""
   action :run
   only_if do
-    `/usr/bin/mysql -u root -p#{node['slurm_test_cluster']['mysql_password']} -D mysql -r -B -N -h 127.0.0.1 -e "SELECT COUNT(*) FROM user WHERE USER = 'slurm' AND HOST = 'localhost'"`.to_i.zero?
+    `/usr/bin/mysql -u root -p#{node['slurm_test_cluster']['mysql_password']} -D mysql -r -B -N -h 127.0.0.1 -e "SELECT COUNT(*) FROM user WHERE USER = 'slurm' AND HOST = '127.0.0.1'"`.to_i.zero?
   end
 end
 
@@ -29,10 +29,10 @@ execute 'create slurm grants' do
   command '/usr/bin/mysql -u root ' \
     "-p#{node['slurm_test_cluster']['mysql_password']} " \
     '-D mysql -r -B -N -h 127.0.0.1 -e ' \
-    '"GRANT ALL ON slurm_acct_db TO \'slurm\'@\'localhost\'"'
+    '"GRANT ALL ON slurm_acct_db TO \'slurm\'@\'127.0.0.1\'"'
   action :run
   not_if do
-    `/usr/bin/mysql -u root -p#{node['slurm_test_cluster']['mysql_password']} -D mysql -r -B -N -h 127.0.0.1 -e "SELECT COUNT(*) FROM user WHERE USER = 'slurm' AND HOST = 'localhost'"`.to_i.zero?
+    `/usr/bin/mysql -u root -p#{node['slurm_test_cluster']['mysql_password']} -D mysql -r -B -N -h 127.0.0.1 -e "SELECT COUNT(*) FROM user WHERE USER = 'slurm' AND HOST = '127.0.0.1'"`.to_i.zero?
   end
 end
 
